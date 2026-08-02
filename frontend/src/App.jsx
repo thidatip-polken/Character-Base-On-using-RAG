0fa3a463a3fa9c55c98f4a784d9d471f8b4ba790
thidatip-polken <thidatippolken@gmail.com>
2026-08-03 04:47:19 +0700

    fix(ci): change browser router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import CharacterPage from "./pages/CharacterPage";
 import ChatPage from "./pages/ChatPage";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Character-Base-On-using-RAG">
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/characters" element={<CharacterPage />} />
